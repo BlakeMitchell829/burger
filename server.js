bf27a2f03c9716b23565b25c23fa57032db53386
Blake Mitchell <38139493+BlakeMitchell829@users.noreply.github.com>
2018-10-15 21:06:45 -0500

    test

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const PORT = process.env.PORT || 3000;
+const app = express();
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(__dirname + "/public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const routes = require("./controllers/burgersController.js");
+
+app.use("/", routes);
+app.use("/update", routes);
+app.use("/create", routes);
+
+
+app.listen(PORT, function() {
+  console.log("24k magic happens on port: " + PORT);
+});
